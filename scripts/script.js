6716f8c6c64014e29c9078beb3fc2bdf3f604bdb
tanyane <63342802+tanyane@users.noreply.github.com>
2023-08-14 10:37:21 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
-document.body.onload = function() {
+document.body.onload = function () {
     setTimeout(() => {
         preloader = document.getElementById("preloader")
         main = document.getElementById("main")
+        indextext = document.getElementById("index-text")
+        text = document.getElementById("index-text-down")
         preloader.style = "display: none;"
-        main.style = "display: block;"
+        main.style = "display: flex; animation: 1000ms opac forwards;"
+        setTimeout(() => {
+            indextext.style = "animation: 1000ms indextext forwards;"
+            text.style = "animation: 1000ms text forwards;"
+        }, 500);
+
+
     }, 1000);
 }
 
@@ -11,13 +19,17 @@ document.body.onload = function() {
 logo = document.getElementById("logo")
 // console.log(logo)
 
-logo.onmouseover = function () {
+// logo.onmouseover = function () {
     // console.log(1)
-    logo.style = "animation: 0.5s logo infinite alternate";
-}
+//     logo.style = "animation: 0.5s logo infinite alternate";
+// }
 
-logo.onmouseout = function () {
-    // console.log(0)
-    logo.style = "animation: 0 logo infinite alternate";
-}
+// logo.onmouseout = function () {
+//     // console.log(0)
+//     logo.style = "animation: 0 logo infinite alternate";
+// }
+
+// photo = document.getElementById("photo")
+
+// console.log(photo)
 
